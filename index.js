732c5d2ffb019d62d9b4bfa4942172df6a5f99dc
Mayuresh1134 <mspednekar6603@gmail.com>
2024-06-03 13:38:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello from node API server");
+});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:F5VeExui8N1gPUAi@backenddb.4wzitbd.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("connection failed!", error);
+  });
